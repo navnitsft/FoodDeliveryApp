5b5e310e695400ca862b787c6a912355b6056b16
navnitsft <navnitsft@gmail.com>
2025-09-07 15:08:06 +0530

    Initial to Navbar code commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Nav from '../components/Nav'
+
+
+function Home() {
+  return (
+    <div className='w-full h-screen bg-slate-200'>
+      <Nav />
+    </div>
+  )
+}
+
+export default Home
