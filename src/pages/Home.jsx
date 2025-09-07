3a375a8f8d638ce7ce89d558fe21b91c09d39561
navnitsft <navnitsft@gmail.com>
2025-09-07 16:29:59 +0530

    categories page created

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import Nav from '../components/Nav'
+import { categories } from './Categories'
 
 
 function Home() {
   return (
     <div className='w-full h-screen bg-slate-200'>
       <Nav />
+      <div className='flex flex-wrap justify-center items-center gap-6 w-full'>
+        {categories.map((category) => (
+          <div key={category.id} className='flex flex-col justify-center items-center gap-2 border-green-600 rounded-lg p-2 bg-white font-semibold text-gray-600 shadow-xl hover:scale-105 transition-transform cursor-pointer'>            
+            {category.icon}
+            {category.name}
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
